74c15802617a715976056c4fe5f37bf6e261f66e
Mohsin Ramzan <mohsinramzan999@gmail.com>
2026-01-14 18:15:48 +0500

    Fixing deployment issues

--- vercel_wsgi.py
+++ vercel_wsgi.py
@@ -1,3 +1,10 @@
-from TWS.TWS.wsgi import application
+import os
+import sys
 
-app = application
+# Add Django project directory to path
+sys.path.append(os.path.join(os.path.dirname(__file__), "TWS"))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TWS.settings")
+
+from django.core.wsgi import get_wsgi_application
+app = get_wsgi_application()
