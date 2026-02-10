3968bee9b77acd630d1d39c5e1d08342be4cce72
Mohsin Ramzan <mohsinramzan999@gmail.com>
2026-02-10 13:52:18 +0500

    Fixing the crash down issue

--- TWS/TWS/settings.py
+++ TWS/TWS/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ri2=d%9x-=q#t9y!htos-ka1*v9c7byt5ei5728^c^g%dt5$3#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
