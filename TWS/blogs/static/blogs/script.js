1eb75b10010e113b017d096c25f79587f31a1a6a
Mohsin Ramzan <mohsinramzan999@gmail.com>
2026-07-30 22:01:57 +0500

    Implementing Autoupdate for the number of blogs in impact section for home page

--- TWS/blogs/static/blogs/script.js
+++ TWS/blogs/static/blogs/script.js
@@ -12,3 +12,5 @@ checkbox.addEventListener("change", () => {
   }
 });
 
+
+
--- TWS/ngo/views.py
+++ TWS/ngo/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-
+from pathlib import Path
+from django.conf import settings
 # Create your views here.
 def about(request):
     return render(request, 'ngo/about.html')
 
 
 def home(request):
-    return render (request,'ngo/home.html')
+
+    blog_dir = Path(settings.BASE_DIR) / "blogs/templates/blogs/blogs_pages"
+    blog_count = len(list(blog_dir.glob("*.html")))-1
+    print("Blog Count:", blog_count)
+    return render(request, 'ngo/home.html', {'blog_count': blog_count})
 
 def contact(request):
     return render(request,'ngo/contact.html')
